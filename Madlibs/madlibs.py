064ec542af727da3e8ab7629e1d378913805f56d
tarushgaur07 <theatomicdestroyer22@gmail.com>
2026-06-26 15:38:37 +0530

    Madlibs project

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,11 @@
+name = input("enter your name:").capitalize()
+birth_date = input("enter your birthdate:")
+birth_place = input("enter your birthplace:")
+hobby = input("enter your hobby:")
+highschool = input("enter your highschool:")
+age = int(input("enter your age:"))
+print("Tittle: My Autobiography \n")
+print(f"""Hi, I'm {name}.I was born on {birth_date} in {birth_place}.
+I love {hobby} , and its been my passion since I was a kid.
+I went to {highschool} and completed my 10th with good grades.
+Now , I'm {age} years old and am studying in college.""")
